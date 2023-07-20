5f2b0352ecd82ba2560cbf49cc9436b72ef9c6aa
Ayush Pandey <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-07-20 11:56:22 +0530

    hmm ok

--- handler.js
+++ handler.js
@@ -727,8 +727,8 @@ export async function participantsUpdate({ id, participants, action }) {
                   ppgp = await this.profilePictureUrl(id, 'image');
                 } catch (error) {
                   console.error(`Error retrieving profile picture: ${error}`);
-                  pp = 'https://i.imgur.com/8B4jwGq.jpeg'; // Assign default image URL
-                  ppgp = 'https://i.imgur.com/8B4jwGq.jpeg'; // Assign default image URL
+                  pp = 'https://i.ibb.co/Ms4Qsvr/IMG-20230719-WA0090.jpg'; // Assign default image URL
+                  ppgp = 'https://i.ibb.co/Ms4Qsvr/IMG-20230719-WA0090.jpg'; // Assign default image URL
                 } finally {
                   let text = (chat.sBye || this.bye || conn.bye || 'HELLO, @user')
                     .replace('@user', '@' + user.split('@')[0]);
